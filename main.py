a0fd8056cfefa66bd9c14649e9dc102c64c80ed5
marcusguerra <13viniciusmarcus@gmail.com>
2022-08-16 01:50:22 -0300

    base

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import time
+import random
+import math
+import sys
+
+pessoas = [('Lisbon', 'LIS'),
+           ('Madrid', 'MAD'),
+           ('Paris', 'CDG'),
+           ('Dublin', 'DUB'),
+           ('Brussels', 'BRU'),
+           ('London', 'LHR')] #locais de saida e as siglas dos aeroportos
+
+destino = 'FCO'
