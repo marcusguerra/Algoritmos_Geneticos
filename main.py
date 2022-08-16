a48d846377365bfcd2dad0bd1483e51254844ea7
marcusguerra <13viniciusmarcus@gmail.com>
2022-08-16 02:05:27 -0300

    leitura do arquivo de voo

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#flights.txt
+#coluna 1- aeoroporto de saida
+#coluna 2- aeroporto de chegada
+#coluna 3- horario saida
+#coluna 4- horario chegada
+#coluna 5- preço
 import time
 import random
 import math
@@ -8,6 +14,14 @@ pessoas = [('Lisbon', 'LIS'),
            ('Paris', 'CDG'),
            ('Dublin', 'DUB'),
            ('Brussels', 'BRU'),
-           ('London', 'LHR')] #locais de saida e as siglas dos aeroportos
+           ('London', 'LHR')]
+#locais de saida e as siglas dos aeroportos
 
 destino = 'FCO'
+
+voos = {}
+for linha in open('flights.txt'):
+    origem, destino, partida, chegada, preco = linha.split(',')
+    voos.setdefault((origem, destino), [])
+    voos[(origem, destino)].append((partida, chegada, int(preco)))
+print(voos['FCO', 'LIS'])
